56d8e11a5aef74d2a93bd49c3f9d287a91d246af
Caio Gomes Braga <45905947+caiobraga@users.noreply.github.com>
2023-08-23 21:19:44 -0300

    Initial commit

---
+++ src/pages/login.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import type { ReactElement } from 'react'
+import Head from 'next/head'
+import Button from '../components/Button'
+import CardBox from '../components/CardBox'
+import SectionFullScreen from '../components/Section/FullScreen'
+import LayoutGuest from '../layouts/Guest'
+import { Field, Form, Formik } from 'formik'
+import FormField from '../components/Form/Field'
+import FormCheckRadio from '../components/Form/CheckRadio'
+import Divider from '../components/Divider'
+import Buttons from '../components/Buttons'
+import { useRouter } from 'next/router'
+import { getPageTitle } from '../config'
+
+export default function Error() {
+  const router = useRouter()
+
+  const handleSubmit = () => {
+    router.push('/dashboard')
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{getPageTitle('Login')}</title>
+      </Head>
+
+      <SectionFullScreen bg="purplePink">
+        <CardBox className="w-11/12 md:w-7/12 lg:w-6/12 xl:w-4/12 shadow-2xl">
+          <Formik
+            initialValues={{ login: 'john.doe', password: 'bG1sL9eQ1uD2sK3b', remember: true }}
+            onSubmit={() => handleSubmit()}
+          >
+            <Form>
+              <FormField label="Login" help="Please enter your login">
+                <Field name="login" />
+              </FormField>
+
+              <FormField label="Password" help="Please enter your password">
+                <Field name="password" type="password" />
+              </FormField>
+
+              <FormCheckRadio type="checkbox" label="Remember">
+                <Field type="checkbox" name="remember" />
+              </FormCheckRadio>
+
+              <Divider />
+
+              <Buttons>
+                <Button type="submit" label="Login" color="info" />
+                <Button href="/dashboard" label="Home" color="info" outline />
+              </Buttons>
+            </Form>
+          </Formik>
+        </CardBox>
+      </SectionFullScreen>
+    </>
+  )
+}
+
+Error.getLayout = function getLayout(page: ReactElement) {
+  return <LayoutGuest>{page}</LayoutGuest>
+}
